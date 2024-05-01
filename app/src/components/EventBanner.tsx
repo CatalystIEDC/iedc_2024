214a4968dc66d8ead5e25c2ebc8492b7508fc6e6
Ferwin Lopez <ferwinlopezt@gmail.com>
2024-05-01 15:47:20 +0530

    feat :  event banner component

--- app/src/components/EventBanner.tsx
+++ app/src/components/EventBanner.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link';
 import React from 'react'
+import Marquee from "react-fast-marquee";
 
-const EventBanner = () => {
+const EventBanner = ({event}:{event:{name:string,details:string}}) => {
   return (
-    <div>EventBanner</div>
+    <div className='w-full py-1 bg-gradient-to-r from-[#FFFFFF00] from-0% via-[#FFFFFF] via-50% to-[#FFFFFF00] to-100% flex items-center hover:py-2 transition-all ease-in-out duration-150 '>
+        <Marquee autoFill={true}  speed={90}>
+        <Link href={'/'} className='text-black font-medium text-base mx-5'>{event.details}</Link>
+        </Marquee>
+    </div>
   )
 }
 
