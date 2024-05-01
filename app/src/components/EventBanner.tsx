ae7803900131327f77e2b46d91e84c9f1712683f
Ferwin Lopez <ferwinlopezt@gmail.com>
2024-05-01 19:05:58 +0530

    feat : navbar

--- app/src/components/EventBanner.tsx
+++ app/src/components/EventBanner.tsx
@@ -4,11 +4,11 @@ import Marquee from "react-fast-marquee";
 
 const EventBanner = ({event}:{event:{name:string,details:string}}) => {
   return (
-    <div className='w-full py-1 bg-gradient-to-r from-[#FFFFFF00] from-0% via-[#FFFFFF] via-50% to-[#FFFFFF00] to-100% flex items-center hover:py-2 transition-all ease-in-out duration-150 '>
+    <Link href={'/'} className='w-full py-1 bg-gradient-to-r from-[#FFFFFF00] from-0% via-[#FFFFFF] via-50% to-[#FFFFFF00] to-100% flex items-center hover:py-2 transition-all ease-in-out duration-200 '>
         <Marquee autoFill={true}  speed={90}>
-        <Link href={'/'} className='text-black font-medium text-base mx-5'>{event.details}</Link>
+        <div  className='text-black font-medium text-base mx-5'>{event.details}</div>
         </Marquee>
-    </div>
+    </Link>
   )
 }
 
--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import Logo from '@/public/Logo/Header logo.svg'
+import Image from 'next/image'
+import Link from 'next/link'
+import {spaceGrotesk} from '@/app/src/utils/fonts'
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Navbar = () => {
+    const links = [
+        {
+            page : 'Home',
+            path : '/'
+        },
+        {
+            page : 'Events',
+            path : '/events'
+        },
+        {
+            page : 'Team',
+            path : '/team'
+        },
+        {
+            page : 'Innovate',
+            path : '/innovate'
+        },
+        {
+            page : 'MuLearn',
+            path : '/mulearn'
+        },
+        {
+            page : 'About',
+            path : '/about'
+        }
+
+    ]
   return (
-    <div>Navbar</div>
+    <div className='flex sticky z-10 top-0 w-full py-4 px-20 justify-between items-center bg-[#0B0B0B] text-white '>
+        <Image src={Logo} alt='Catalyst Logo' width={45} height={45} className='object-cover'/>
+        <div className=' gap-12 hidden lg:flex '>
+         {
+            links.map((link,index)=>{
+                return (
+                    <Link href={link.path} className={`${spaceGrotesk.className} md:text-base lg:text- font-normal text-center `}>{link.page}</Link>
+                )
+            })
+         }
+        </div>
+        <HiOutlineMenuAlt3 size={30} className='lg:hidden'/>
+    </div>
   )
 }
 
