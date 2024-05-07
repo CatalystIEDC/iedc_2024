7ddf4973fbeb9c470e1e7ad272d5ba8a61f8cce9
Ferwin Lopez <ferwinlopezt@gmail.com>
2024-05-07 22:54:16 +0530

    feat :  team data and photos

---
+++ app/utils/team.js
@@ -0,0 +1,246 @@
+const team ={
+  
+        "executive": [
+            {
+                "name": "Arun J S",
+                "designation": "Nodal Officer"
+            },
+            {
+                "name": "C Rachana",
+                "designation": "Chief Executive Officer"
+            },
+            {
+                "name": "Maria Sara Raju",
+                "designation": "Executive Co-Lead"
+            },
+            {
+                "name": "Rahul G Paul",
+                "designation": "Executive Co-Lead"
+            }
+        ],
+        "operations": [
+            {
+                "name": "Krishnapriya Deepak",
+                "designation": "Chief Operation Officer"
+            },
+            {
+                "name": "Silpa C",
+                "designation": "Quality and Operations Co-Lead"
+            },
+            {
+                "name": "Advaith Manoj",
+                "designation": "Quality and Operations Co-Lead"
+            }
+        ],
+        "skills": [
+            {
+                "name": "Aditya S",
+                "designation": "Chief Skills Officer"
+            },
+            {
+                "name": "Hannath Sajad",
+                "designation": "Skill Development Co-Lead"
+            },
+            {
+                "name": "Niranj R",
+                "designation": "Skill Development Co-Lead"
+            }
+        ],
+        "tech": [
+            {
+                "name": "Ferwin Lopez",
+                "designation": "Chief Technical Officer"
+            },
+            {
+                "name": "Abhishek Raman",
+                "designation": "Technical Strategist- Web Development"
+            },
+            {
+                "name": "Gourisankar S",
+                "designation": "Technical Strategist- Hardware and IEDC Lab Projects"
+            }
+        ],
+        "marketing": [
+            {
+                "name": "Bharat Raj S",
+                "designation": "Chief Marketing Officer"
+            },
+            {
+                "name": "Sona Ponnachen",
+                "designation": "Marketing Co-Lead-Branding"
+            },
+            {
+                "name": "Vasundhara S R",
+                "designation": "Marketing Co-Lead-Branding-Content"
+            }
+        ],
+        "creative": [
+            {
+                "name": "Diya S",
+                "designation": "Chief Creative Officer"
+            },
+            {
+                "name": "Vinisha S",
+                "designation": "Creativity and Innovations Co-Lead"
+            },
+            {
+                "name": "Sradhya Renish",
+                "designation": "Creativity and Innovations Co-Lead"
+            }
+        ],
+        "finance": [
+            {
+                "name": "Adlu Rahman",
+                "designation": "Chief Finance Officer"
+            },
+            {
+                "name": "Neil John",
+                "designation": "Finance Strategist"
+            },
+            {
+                "name": "Abhiram S",
+                "designation": "Finance Strategist"
+            }
+        ],
+        "vibe": [
+            {
+                "name": "Abhiram Krishna",
+                "designation": "Chief Vibe Officer"
+            },
+            {
+                "name": "Rachel Antony",
+                "designation": "Vibe Officer"
+            },
+            {
+                "name": "J Sidharth",
+                "designation": "Vibe Officer"
+            }
+        ],
+        "iic": [
+            {
+                "name": "Anoushka L Nair",
+                "designation": "IIC Lead"
+            },
+            {
+                "name": "Ann Santhosh",
+                "designation": "IIC Operations Lead"
+            },
+            {
+                "name": "Abhilash George",
+                "designation": "IIC Startups Lead"
+            },
+            {
+                "name": "Nandhana V",
+                "designation": "IIC Marketing Lead"
+            }
+        ],
+        "mulearn": [
+            {
+                "name": "Aditya S",
+                "designation": "MuLearn Lead"
+            },
+            {
+                "name": "Vinisha S",
+                "designation": "MuLearn Co-Lead"
+            },
+            {
+                "name": "Agnivesh P S",
+                "designation": "Design Lead"
+            },
+            {
+                "name": "Abhishek Raman",
+                "designation": "Tech Lead"
+            },
+            {
+                "name": "E V Jishnu",
+                "designation": "Media Lead"
+            },
+            {
+                "name": "S Blessy Catherine",
+                "designation": "Content Lead"
+            },
+            {
+                "name": "Abhishek Raman",
+                "designation": "Web Dev- Frontend IG Lead"
+            },
+            {
+                "name": "Johann George",
+                "designation": "Web Dev- Backend IG Lead"
+            },
+            {
+                "name": "Prajwal P P",
+                "designation": "UI/UX IG Lead"
+            },
+            {
+                "name": "Shibin Shibu",
+                "designation": "UI/UX IG Lead"
+            },
+            {
+                "name": "Gourisankar S",
+                "designation": "IOT IG Lead"
+            },
+            {
+                "name": "Aditya S",
+                "designation": "IOT IG Lead"
+            },
+            {
+                "name": "Anand Pillai",
+                "designation": "APP DEV- Flutter and AI IG Lead"
+            }
+        ],
+        "tribe": [
+            {
+                "name": "Shibin Shibhu",
+                "designation": "Head Of Operations"
+            },
+            {
+                "name": "Rahul V",
+                "designation": "Creative Director"
+            },
+            {
+                "name": "Navaneeth M S",
+                "designation": "Visual Media Specialist"
+            },
+            {
+                "name": "Adwaith A S",
+                "designation": "Visual Media Specialist"
+            },
+            {
+                "name": "Agnivesh P S",
+                "designation": "DAVINCIS Collective"
+            },
+            {
+                "name": "Jefry Mammen",
+                "designation": "DAVINCIS Collective"
+            },
+            {
+                "name": "Adwaith A Arun",
+                "designation": "DAVINCIS Collective"
+            },
+            {
+                "name": "Sabareesh P R",
+                "designation": "DAVINCIS Collective"
+            },
+            {
+                "name": "Lekha R",
+                "designation": "Creative Curator"
+            },
+            {
+                "name": "Jason E",
+                "designation": "Creative Curator"
+            },
+            {
+                "name": "Devika S H",
+                "designation": "Emerging Professional"
+            },
+            {
+                "name": "Feba Mariyam Jacob",
+                "designation": "Emerging Professional"
+            },
+            {
+                "name": "Nikitha A R",
+                "designation": "Emerging Professional"
+            }
+        ]
+    }
+
